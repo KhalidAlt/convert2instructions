27ebfa529314394b5e554a30bb93de894aa7e524
Khalid Almubarak <49135754+KhalidAlt@users.noreply.github.com>
2023-03-17 23:11:44 +0300

    Update register_oscar.py

--- register_oscar.py
+++ register_oscar.py
@@ -20,7 +20,7 @@ def mask_words(sentence, prob_mask=0.1):
         str: The sentence with the masked words.
     """
     words = sentence.split(' ')
-    n = n = round(len(words) * prob_mask)
+    n = round(len(words) * prob_mask)
     maskedw = ['The missing words:']
     for i in random.sample(range(len(words)), n):
         maskedw.append(f'{words[i]},')
@@ -44,7 +44,7 @@ def mask_sentence(sentence, prob_mask=0.1):
     """
     """
     words = sentence.split('.')
-    n = n = round(len(words) * prob_mask)
+    n = round(len(words) * prob_mask)
     masked_sen = []
     for i in random.sample(range(len(words)), n):
         masked_sen.append(words[i])
@@ -66,7 +66,6 @@ def mask_paragraph(sentence):
     """
     """
     words = sentence.split('\n')[:-1]
-    n = n = round(len(words) * prob_mask)
     
     i = random.sample(range(len(words)),1)[0]
     missing = words[i]
